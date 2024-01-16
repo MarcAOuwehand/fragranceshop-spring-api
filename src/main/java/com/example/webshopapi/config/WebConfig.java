3dd00d2ae875cd6c7e4bb87e535d928153c24bfe
Marc Ouwehand <1142622@student.hsleiden.nl>
2024-01-16 16:02:33 +0100

    Foutje

--- src/main/java/com/example/webshopapi/config/WebConfig.java
+++ src/main/java/com/example/webshopapi/config/WebConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
         @Override
         public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:4200") // Hier kun je meerdere origins toevoegen als nodig.
+                    .allowedOrigins("http://localhost:4200", "https://seashell-app-xnfcn.ondigitalocean.app", "https://s1142622-iprwc.store:8081", "https://178.62.238.163:8081") // Hier kun je meerdere origins toevoegen als nodig.
                     .allowedMethods("GET", "POST", "PUT", "DELETE") // Als voorbeeld
                     .allowCredentials(true);
         }
